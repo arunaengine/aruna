7d1d2b687355d87e679dcd0de91d7cf839e03a7b
Sebastian Beyvers <sb@pus.de>
2025-04-14 16:02:26 +0200

    feat: Added draft for main

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Aruna V3 federated architecture
+
+
+This MUST! (for you Lukas) contain subcrates for:
+
+- data
+- metadata
+- auth
+- api
+- aruna (main crate)
---
+++ aruna/src/main.rs
@@ -0,0 +1,14 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+
+use aruna_net::connection_handler::ConnectionHandlerBuilder;
+
+#[tokio::main]
+pub async fn main() {
+    let chandler = ConnectionHandlerBuilder::new(None)
+        .add_bind_addr_v4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 31337))
+        .build(vec![])
+        .await
+        .unwrap();
+
+    //transfer_manager::init(chandler.clone()).await.unwrap();
+}
