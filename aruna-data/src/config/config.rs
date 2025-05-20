6cc0d8c46d434f2b8bce583348c43b78045ff193
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-05-20 13:34:34 +0200

    feat: Extend config

--- aruna-data/src/config/config.rs
+++ aruna-data/src/config/config.rs
@@ -1,12 +1,21 @@
-use std::collections::HashMap;
-
+use s3s::S3Result;
+use s3s::host::{S3Host, VirtualHost};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 use crate::util::opendal::Backend;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
+    pub persistence: Persistence,
     pub backend: BackendConfig,
+    pub frontend: Frontend,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub struct Persistence {
+    pub path: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,4 +26,18 @@ pub struct BackendConfig {
     pub compression: bool,
     pub deduplication: bool,
     pub access: HashMap<String, String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub struct Frontend {
+    pub server: String,
+    pub hostname: String,
+    pub cors_exception: Option<String>,
+}
+
+impl S3Host for Frontend {
+    fn parse_host_header<'a>(&'a self, host: &'a str) -> S3Result<VirtualHost<'a>> {
+        Ok(VirtualHost::new(self.server.clone()))
+    }
 }
