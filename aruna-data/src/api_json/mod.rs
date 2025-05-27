d2772248611688847842f503f95687c049fbe8ba
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-05-27 14:55:55 +0200

    feat: Integrate basic openapi support

---
+++ aruna-data/src/api_json/mod.rs
@@ -0,0 +1,6 @@
+pub mod openapi;
+mod paths;
+mod requests;
+pub mod server;
+mod util;
+pub mod request;
---
+++ aruna-data/src/api_json/util.rs
@@ -0,0 +1,17 @@
+use crate::error::ArunaDataError;
+use axum::{Json, http::HeaderMap, response::IntoResponse};
+use serde::Serialize;
+
+pub(super) fn extract_token(header: &HeaderMap) -> Option<String> {
+    header
+        .get("Authorization".to_string())
+        .and_then(|v| v.to_str().ok())
+        .and_then(|v| v.strip_prefix("Bearer "))
+        .map(|v| v.to_string())
+}
+
+pub fn into_axum_response<T: Serialize>(response: Result<T, ArunaDataError>) -> impl IntoResponse {
+    response
+        .map(|r| (axum::http::StatusCode::OK, Json(r)).into_response())
+        .unwrap_or_else(|e| e.into_axum_tuple().into_response())
+}
