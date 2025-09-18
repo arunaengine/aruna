b48966ea20083488db8b97d8118f4e7be2b7653c
Lukas Brehm <97600985+lfbrehm@users.noreply.github.com>
2025-09-18 14:11:56 +0200

    fix: UserIdentity conversion to string

--- aruna-metadata/src/models/conversions.rs
+++ aruna-metadata/src/models/conversions.rs
@@ -244,7 +244,7 @@ pub mod autosurgeon_user_identity {
         identity: &UserIdentity,
         mut reconciler: R,
     ) -> Result<(), R::Error> {
-        reconciler.bytes(identity.to_bytes())
+        reconciler.str(identity.to_string())
     }
 }
 
