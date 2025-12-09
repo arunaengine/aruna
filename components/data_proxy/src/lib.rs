42a7f117bf8a9c95e33a9effc39d709fb91fda38
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-12-09 09:12:14 +0100

    feat: Add lib.rs to Dataproxy

---
+++ components/data_proxy/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod auth;
+pub mod bundler;
+pub mod caching;
+pub mod config;
+pub mod data_backends;
+pub mod database;
+pub mod grpc_api;
+pub mod helpers;
+#[macro_use]
+pub mod macros;
+pub mod replication;
+pub mod s3_frontend;
+pub mod structs;
