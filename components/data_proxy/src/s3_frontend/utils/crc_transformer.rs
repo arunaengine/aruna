05001eba58128116209b471739974a83ca182074
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2025-12-17 23:04:59 +0100

    test: Add unit tests to the CrcTransformer (#218)

--- components/data_proxy/src/s3_frontend/utils/crc_transformer.rs
+++ components/data_proxy/src/s3_frontend/utils/crc_transformer.rs
@@ -205,3 +205,227 @@ where
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::s3_frontend::utils::crc_transformer::CrcTransformer;
+    use crc_fast::{CrcAlgorithm, Digest as CrcDigest};
+    use digest::Digest;
+    use pithos_lib::helpers::notifications::Message;
+    use pithos_lib::helpers::structs::{EncryptionKey, FileContext};
+    use pithos_lib::readwrite::GenericReadWriter;
+    use pithos_lib::transformer::ReadWriter;
+    use pithos_lib::transformers::footer::FooterGenerator;
+    use pithos_lib::transformers::hashing_transformer::HashingTransformer;
+    use pithos_lib::transformers::pithos_comp_enc::PithosTransformer;
+    use pithos_lib::transformers::size_probe::SizeProbe;
+    use sha2::Sha256;
+
+    #[tokio::test]
+    async fn test_transformer_crc32() {
+        let input = b"This is some super important data".to_vec();
+        let mut output = Vec::new();
+
+        let mut asr = GenericReadWriter::new_with_writer(input.as_ref(), &mut output);
+        let (crc32_transformer, crc32_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32IsoHdlc), false);
+
+        asr = asr.add_transformer(crc32_transformer);
+        asr.process().await.unwrap();
+
+        let crc32 = crc32_recv.try_recv().unwrap();
+        assert_eq!(crc32, "6a21ac65")
+    }
+
+    #[tokio::test]
+    async fn test_transformer_crc32c() {
+        let input = b"This is some super important data".to_vec();
+        let mut output = Vec::new();
+
+        let mut asr = GenericReadWriter::new_with_writer(input.as_ref(), &mut output);
+        let (crc32c_transformer, crc32c_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32Iscsi), false);
+
+        asr = asr.add_transformer(crc32c_transformer);
+        asr.process().await.unwrap();
+
+        let crc32c = crc32c_recv.try_recv().unwrap();
+
+        assert_eq!(crc32c, "271fd1c3")
+    }
+
+    #[tokio::test]
+    async fn test_transformer_crc64nvme() {
+        let input = b"This is some super important data".to_vec();
+        let mut output = Vec::new();
+
+        let mut asr = GenericReadWriter::new_with_writer(input.as_ref(), &mut output);
+        let (crc64nvme_transformer, crc64nvme_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc64Nvme), false);
+
+        asr = asr.add_transformer(crc64nvme_transformer);
+        asr.process().await.unwrap();
+
+        let crc64nvme = crc64nvme_recv.try_recv().unwrap();
+
+        assert_eq!(crc64nvme, "9c6658ea321baf04")
+    }
+
+    #[tokio::test]
+    async fn test_transformer_larger_input() {
+        let input = b"abcde".repeat(1234567).to_vec();
+        let mut output = Vec::new();
+
+        let mut asr = GenericReadWriter::new_with_writer(input.as_ref(), &mut output);
+        let (crc32_transformer, crc32_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32IsoHdlc), false);
+        let (crc32c_transformer, crc32c_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32Iscsi), false);
+        let (crc64nvme_transformer, crc64nvme_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc64Nvme), false);
+        asr = asr.add_transformer(crc32_transformer);
+        asr = asr.add_transformer(crc32c_transformer);
+        asr = asr.add_transformer(crc64nvme_transformer);
+        asr.process().await.unwrap();
+
+        let crc32 = crc32_recv.try_recv().unwrap();
+        let crc32c = crc32c_recv.try_recv().unwrap();
+        let crc64nvme = crc64nvme_recv.try_recv().unwrap();
+
+        assert_eq!(crc32, "3746087e");
+        assert_eq!(crc32c, "871b47d3");
+        assert_eq!(crc64nvme, "b6275d2fb6d88d11")
+    }
+
+    #[tokio::test]
+    async fn test_transformer_multifile() {
+        let file1 = b"Lorem ipsum dolor sit amet, consetetur sadipscing elitr.".to_vec();
+        let file2 = b"Stet clita kasd gubergren, no sea takimata sanctus.".to_vec();
+        let combined = Vec::from_iter(file1.clone().into_iter().chain(file2.clone()));
+
+        let (sx, rx) = async_channel::bounded(10);
+        sx.send(Message::FileContext(FileContext {
+            file_path: "file1.txt".to_string(),
+            compressed_size: file1.len() as u64,
+            decompressed_size: file1.len() as u64,
+            compression: true,
+            ..Default::default()
+        }))
+            .await
+            .unwrap();
+
+        sx.send(Message::FileContext(FileContext {
+            file_path: "file2.txt".to_string(),
+            compressed_size: file2.len() as u64,
+            decompressed_size: file2.len() as u64,
+            compression: false,
+            ..Default::default()
+        }))
+            .await
+            .unwrap();
+
+        // Create a new GenericReadWriter
+        let mut output: Vec<u8> = Vec::new();
+        let mut asr = GenericReadWriter::new_with_writer(combined.as_ref(), &mut output);
+        asr.add_message_receiver(rx).await.unwrap();
+
+        let (crc32_transformer, crc32_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32IsoHdlc), true);
+        asr = asr.add_transformer(crc32_transformer);
+        asr.process().await.unwrap();
+
+        let crc32_file1 = crc32_recv.try_recv().unwrap();
+        let crc32_file2 = crc32_recv.try_recv().unwrap();
+
+        assert_eq!(crc32_file1, "11382a6b");
+        assert_eq!(crc32_file2, "bb283921");
+    }
+
+    #[tokio::test]
+    async fn test_transformer_multifile_larger() {
+        let file1 = b"abcdefg".repeat(1024 * 1024).to_vec();
+        let file2 = b"zyxwvu".repeat(1024 * 1024).to_vec();
+        let combined = Vec::from_iter(file1.clone().into_iter().chain(file2.clone()));
+
+        let (sx, rx) = async_channel::bounded(10);
+        sx.send(Message::FileContext(FileContext {
+            file_path: "file1.txt".to_string(),
+            compressed_size: file1.len() as u64,
+            decompressed_size: file1.len() as u64,
+            compression: true,
+            ..Default::default()
+        }))
+            .await
+            .unwrap();
+
+        sx.send(Message::FileContext(FileContext {
+            file_path: "file2.txt".to_string(),
+            compressed_size: file2.len() as u64,
+            decompressed_size: file2.len() as u64,
+            compression: false,
+            ..Default::default()
+        }))
+            .await
+            .unwrap();
+
+        // Create a new GenericReadWriter
+        let mut output: Vec<u8> = Vec::new();
+        let mut asr = GenericReadWriter::new_with_writer(combined.as_ref(), &mut output);
+        asr.add_message_receiver(rx).await.unwrap();
+
+        let (crc32_transformer, crc32_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc32IsoHdlc), true);
+        asr = asr.add_transformer(crc32_transformer);
+        asr.process().await.unwrap();
+
+        let crc32_file1 = crc32_recv.try_recv().unwrap();
+        let crc32_file2 = crc32_recv.try_recv().unwrap();
+
+        assert_eq!(crc32_file1, "48376d21");
+        assert_eq!(crc32_file2, "9de97047");
+    }
+
+    #[tokio::test]
+    async fn test_transformer_proxy_mock() {
+        let input = b"This is some super important data".to_vec();
+        let mut output = Vec::new();
+
+        let mut asr = GenericReadWriter::new_with_writer(input.as_ref(), &mut output);
+        let (uncompressed_probe, _) = SizeProbe::new();
+        let (sha_transformer, _) =
+            HashingTransformer::new_with_backchannel(Sha256::new(), "sha256".to_string());
+        let (crc64nvme_transformer, crc64nvme_recv) =
+            CrcTransformer::new_with_backchannel(CrcDigest::new(CrcAlgorithm::Crc64Nvme), false);
+
+        let (tx, rx) = async_channel::bounded(10);
+        asr.add_message_receiver(rx).await.unwrap();
+
+        tx.send(Message::FileContext(FileContext {
+            idx: 0,
+            file_path: "file1.txt".to_string(),
+            compressed_size: input.len() as u64,
+            decompressed_size: input.len() as u64,
+            compression: false,
+            recipients_pubkeys: vec![],
+            encryption_key: EncryptionKey::Same(
+                b"wvwj3485nxgyq5ub9zd3e7jsrq7a92ea"
+                    .to_vec()
+                    .try_into()
+                    .unwrap(),
+            ),
+            ..Default::default()
+        }))
+            .await
+            .unwrap();
+
+        asr = asr.add_transformer(uncompressed_probe);
+        asr = asr.add_transformer(sha_transformer);
+        asr = asr.add_transformer(crc64nvme_transformer);
+        asr = asr.add_transformer(PithosTransformer::new());
+        asr = asr.add_transformer(FooterGenerator::new(None));
+        asr.process().await.unwrap();
+
+        let crc64nvme = crc64nvme_recv.try_recv().unwrap();
+        assert_eq!(crc64nvme, "9c6658ea321baf04")
+    }
+}
