1f9ce8f1164cccf34fb53b40649de50ffb0160e2
Jannis Schlegel <jannis.hochmuth@computational.bio.uni-giessen.de>
2026-02-25 09:11:52 +0100

    feat: Main function for aruna crate, which currently only starts static S3 server

---
+++ aruna/src/main.rs
@@ -0,0 +1,74 @@
+use aruna_api::s3::s3_server::S3Server;
+use aruna_blob::blob::BlobHandler;
+use aruna_core::structs::Backend::FileSystem;
+use aruna_core::structs::BackendConfig;
+use aruna_operations::driver::DriverContext;
+use aruna_storage::FjallStorage;
+use std::backtrace::Backtrace;
+use std::collections::HashMap;
+use std::panic;
+use std::sync::Arc;
+use tracing::{error, info};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer};
+use ulid::Ulid;
+
+#[tokio::main]
+pub async fn main() {
+    panic::set_hook(Box::new(|info| {
+        let stacktrace = Backtrace::force_capture();
+        println!("Got panic. @info:{info}\n@stackTrace:{stacktrace}");
+        std::process::abort();
+    }));
+
+    let logging_env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or("none".into())
+        .add_directive("aruna_api=trace".parse().unwrap());
+
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .with_filter(logging_env_filter);
+
+    tracing_subscriber::registry().with(fmt_layer).init();
+
+    let storage_handle = FjallStorage::open(&format!("/dev/shm/{}", Ulid::new())).unwrap();
+    let blob_handle = BlobHandler::new(
+        BackendConfig {
+            backend_type: FileSystem,
+            root: "/tmp".to_string(),
+            service_config: HashMap::new(),
+            bucket_prefix: None,
+            max_bucket_size: Some(100000),
+        },
+        storage_handle.clone(),
+    )
+    .await
+    .unwrap();
+
+    let driver_ctx = Arc::new(DriverContext {
+        storage_handle,
+        net_handle: None,
+        blob_handle: Some(blob_handle),
+    });
+
+    let s3_server = S3Server::new("0.0.0.0:1337", "localhost:1337", driver_ctx)
+        .await
+        .unwrap();
+
+    let server_handle = s3_server.run().await.unwrap();
+    tokio::select! {
+        res = server_handle => {
+            match res {
+                Ok(_) => info!("Server stopped normally"),
+                Err(e) => error!("Server panicked: {:?}", e),
+            }
+        }
+        // You can add other signals here, e.g., Ctrl+C
+        _ = tokio::signal::ctrl_c() => {
+            info!("Shutting down S3 interface");
+        }
+    }
+}
