8b2b92231f5736b06ef74a72ae9717d79472e246
Sebastian Beyvers <sb@pus.de>
2026-05-06 11:44:27 +0200

    chore: Clear out repo to prepare for v3 migration

--- components/data_proxy/src/s3_frontend/utils/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod buffered_s3_sink;
-pub mod checksum;
-pub mod crc_transformer;
-pub mod debug_transformer;
-pub mod list_objects;
-pub mod metadata;
-pub mod ranges;
-pub mod replication_sink;
