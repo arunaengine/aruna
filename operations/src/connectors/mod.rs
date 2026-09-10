416af842ffca896315a9f2632fdcc7daf8ab92ce
Sebastian Beyvers <sb@pus.de>
2026-09-10 18:08:15 +0200

    refactor: share connector reference scan

--- operations/src/connectors/mod.rs
+++ operations/src/connectors/mod.rs
@@ -3,6 +3,7 @@ pub mod delete_source_connector;
 pub mod get_source_connector;
 pub mod has_secret_config;
 pub mod list_source_connectors;
+pub mod reference_scan;
 pub mod replace_source_connector;
 pub mod repository;
 pub mod resolver;
