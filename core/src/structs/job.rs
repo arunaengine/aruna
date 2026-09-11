3155bb521f1308c40fd85787cec1f8d366520571
Sebastian Beyvers <sb@pus.de>
2026-09-11 16:18:52 +0000

    refactor: rename core modules

--- core/src/structs/job.rs
+++ core/src/structs/job.rs
@@ -11,14 +11,14 @@ use crate::NodeId;
 use crate::compute::ExecutionTargetId;
 use crate::compute::runtimes::{SESSION_RUNTIME_TAG, SESSION_TAG, SESSION_TAG_NOTEBOOK};
 use crate::errors::ConversionError;
+use crate::identifiers::{
+    BucketId, FieldError, JobId as RoutableJobId, PlacementHandle, StructuredId,
+};
 use crate::structs::invert_timestamp_ms;
 use crate::structs::{
     AuthContext, BackendLocation, HarvestJobSpec, HiddenBlobKey, MintPersistentIdSpec,
     PlacementPolicyRef, PlacementRef, RealmId, StagingStrategy, StoragePurgeResult,
     StoragePurgeScope, StoragePurgeSpec,
-};
-use crate::structured_id::{
-    BucketId, FieldError, JobId as RoutableJobId, PlacementHandle, StructuredId,
 };
 use crate::types::{GroupId, Key, UserId};
 use crate::util::tail_str;
