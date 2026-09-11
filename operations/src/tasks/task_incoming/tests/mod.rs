5667dd1f1aeb99b3240991e1e92587d6d3a0ae07
Sebastian Beyvers <sb@pus.de>
2026-09-11 03:55:58 +0000

    refactor: drop redundant test module types

--- operations/src/tasks/task_incoming/tests/mod.rs
+++ operations/src/tasks/task_incoming/tests/mod.rs
@@ -10,8 +10,6 @@ use aruna_core::document::{
     DocumentSyncChange, DocumentSyncChangeKind, DocumentSyncOutboxEvent, DocumentSyncOutboxRecord,
     DocumentSyncRevision,
 };
-use aruna_core::effects::StorageEffect;
-use aruna_core::events::StorageEvent;
 use aruna_core::keyspaces::{
     METADATA_GRAPH_PRUNE_JOB_KEYSPACE, NOTIFICATION_INBOX_KEYSPACE, TASK_TIMER_KEYSPACE,
 };
@@ -19,7 +17,7 @@ use aruna_core::metadata::{MetadataGraphLifecycleRecord, MetadataGraphPruneJobRe
 use aruna_core::storage_entries::notification_outbox_write_entry;
 use aruna_core::structs::{
     Actor, FIRST_GRANTABLE_HANDLE, JobId, JobPayload, JobRecord, JobState, NotificationClass,
-    NotificationKind, NotificationOutboxRecord, RealmConfigDocument, RealmId, RealmNodeKind,
+    NotificationKind, NotificationOutboxRecord, RealmNodeKind,
 };
 use aruna_core::structured_id::{BucketId, PlacementHandle};
 use aruna_core::types::UserId;
