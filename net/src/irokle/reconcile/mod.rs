47f9ee012d9f32a4e76ea5775aea56938d1fa154
Sebastian Beyvers <sb@pus.de>
2026-09-11 11:35:30 +0000

    refactor: move admin document dispatch to reconcile

--- net/src/irokle/reconcile/mod.rs
+++ net/src/irokle/reconcile/mod.rs
@@ -1,10 +1,12 @@
 use super::*;
 
+mod admin;
 mod cursor;
 mod materialize;
 mod registry;
 mod validate;
 
+pub(in crate::document_sync) use self::admin::*;
 pub(in crate::document_sync) use self::cursor::*;
 pub(in crate::document_sync) use self::materialize::*;
 pub(in crate::document_sync) use self::registry::*;
