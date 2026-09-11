db183ef3223d06497b956ba7e3034fb846d78b2d
Sebastian Beyvers <sb@pus.de>
2026-09-11 11:33:03 +0000

    refactor: move users resolve fixtures to tests module

--- api/src/routes/users/vault.rs
+++ api/src/routes/users/vault.rs
@@ -210,7 +210,7 @@ pub async fn delete_vault(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::routes::users::resolve_tests::{realm_auth, setup_state};
+    use crate::routes::users::tests::fixtures::{realm_auth, setup_state};
     use aruna_core::structs::MAX_USER_VAULT_BYTES;
     use axum::response::IntoResponse;
 
