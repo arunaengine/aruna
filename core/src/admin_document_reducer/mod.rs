60509b00a723e3095cd0806d72e582b2a9c9c7ab
Sebastian Beyvers <sb@pus.de>
2026-09-11 10:41:04 +0000

    refactor: move user target getters

--- core/src/admin_document_reducer/mod.rs
+++ core/src/admin_document_reducer/mod.rs
@@ -31,6 +31,7 @@ use crate::user_update_validation::{
 mod paths;
 mod placement;
 mod revocation;
+mod targets;
 
 pub use paths::*;
 pub use placement::*;
@@ -888,42 +889,6 @@ impl AdminDocumentReducerState {
         Ok(apply_status)
     }
 
-    pub fn materialized_user_name(&self) -> Option<String> {
-        if !matches!(&self.target, AdminDocumentTarget::User { .. }) {
-            return None;
-        }
-
-        self.user_name
-            .as_ref()
-            .and_then(|version| version.value.clone())
-    }
-
-    pub fn materialized_user_subject_ids(&self) -> BTreeSet<String> {
-        if !matches!(&self.target, AdminDocumentTarget::User { .. }) {
-            return BTreeSet::new();
-        }
-
-        self.user_subject_ids
-            .values()
-            .filter_map(|version| version.value.clone())
-            .collect()
-    }
-
-    pub fn materialized_user_attributes(&self) -> BTreeMap<String, String> {
-        if !matches!(&self.target, AdminDocumentTarget::User { .. }) {
-            return BTreeMap::new();
-        }
-
-        self.user_attributes
-            .iter()
-            .filter_map(|(key, version)| {
-                version
-                    .value
-                    .as_ref()
-                    .map(|value| (key.clone(), value.clone()))
-            })
-            .collect()
-    }
 
     pub fn materialized_group_display_name(&self) -> Option<String> {
         if !matches!(&self.target, AdminDocumentTarget::Group { .. }) {
