0aa08d055ce962b8d6d0369325527ec88e3cda16
Sebastian Beyvers <sb@pus.de>
2026-09-15 15:50:16 +0200

    refactor: shorten operations notifications watch names

--- operations/src/onboarding/delete_secret.rs
+++ operations/src/onboarding/delete_secret.rs
@@ -13,19 +13,19 @@ use crate::onboarding::create_secret::secret_record_key;
 use crate::onboarding::secret_state::secret_state_key;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct DeleteOnboardingSecretInput {
+pub struct DeleteSecretInput {
     pub enrollment_id: Ulid,
 }
 
 #[derive(Debug, PartialEq)]
-pub struct DeleteOnboardingSecretOperation {
-    input: DeleteOnboardingSecretInput,
-    state: DeleteOnboardingSecretState,
-    output: Option<Result<OnboardingSecretRecord, DeleteOnboardingSecretError>>,
+pub struct DeleteSecretOperation {
+    input: DeleteSecretInput,
+    state: DeleteSecretState,
+    output: Option<Result<OnboardingSecretRecord, DeleteSecretError>>,
 }
 
 #[derive(Clone, Debug, PartialEq)]
-enum DeleteOnboardingSecretState {
+enum DeleteSecretState {
     Init,
     StartTransaction,
     ReadRecord {
@@ -43,7 +43,7 @@ enum DeleteOnboardingSecretState {
 }
 
 #[derive(Debug, Error, PartialEq)]
-pub enum DeleteOnboardingSecretError {
+pub enum DeleteSecretError {
     #[error(transparent)]
     StorageError(#[from] StorageError),
     #[error(transparent)]
@@ -60,22 +60,22 @@ pub enum DeleteOnboardingSecretError {
     },
 }
 
-impl DeleteOnboardingSecretOperation {
-    pub fn new(input: DeleteOnboardingSecretInput) -> Self {
+impl DeleteSecretOperation {
+    pub fn new(input: DeleteSecretInput) -> Self {
         Self {
             input,
-            state: DeleteOnboardingSecretState::Init,
+            state: DeleteSecretState::Init,
             output: None,
         }
     }
 }
 
-impl Operation for DeleteOnboardingSecretOperation {
+impl Operation for DeleteSecretOperation {
     type Output = OnboardingSecretRecord;
-    type Error = DeleteOnboardingSecretError;
+    type Error = DeleteSecretError;
 
     fn start(&mut self) -> Effects {
-        self.state = DeleteOnboardingSecretState::StartTransaction;
+        self.state = DeleteSecretState::StartTransaction;
         smallvec![Effect::Storage(StorageEffect::StartTransaction {
             read: false,
         })]
@@ -84,18 +84,18 @@ impl Operation for DeleteOnboardingSecretOperation {
     fn step(&mut self, event: Event) -> Effects {
         let event = match event {
             Event::Storage(StorageEvent::Error { error }) => {
-                return fail(self, DeleteOnboardingSecretError::StorageError(error));
+                return fail(self, DeleteSecretError::StorageError(error));
             }
             other => other,
         };
 
         match self.state.clone() {
-            DeleteOnboardingSecretState::StartTransaction => {
+            DeleteSecretState::StartTransaction => {
                 let got = format!("{event:?}");
                 let Event::Storage(StorageEvent::TransactionStarted { txn_id }) = event else {
                     return fail(
                         self,
-                        DeleteOnboardingSecretError::UnexpectedEvent {
+                        DeleteSecretError::UnexpectedEvent {
                             state: format!("{:?}", self.state),
                             expected: "transaction started",
                             got,
@@ -103,19 +103,19 @@ impl Operation for DeleteOnboardingSecretOperation {
                     );
                 };
 
-                self.state = DeleteOnboardingSecretState::ReadRecord { txn_id };
+                self.state = DeleteSecretState::ReadRecord { txn_id };
                 smallvec![Effect::Storage(StorageEffect::Read {
                     key_space: ONBOARDING_KEYSPACE.to_string(),
                     key: secret_record_key(self.input.enrollment_id),
                     txn_id: Some(txn_id),
                 })]
             }
-            DeleteOnboardingSecretState::ReadRecord { txn_id } => {
+            DeleteSecretState::ReadRecord { txn_id } => {
                 let got = format!("{event:?}");
                 let Event::Storage(StorageEvent::ReadResult { value, .. }) = event else {
                     return fail(
                         self,
-                        DeleteOnboardingSecretError::UnexpectedEvent {
+                        DeleteSecretError::UnexpectedEvent {
                             state: format!("{:?}", self.state),
                             expected: "read result",
                             got,
@@ -124,19 +124,16 @@ impl Operation for DeleteOnboardingSecretOperation {
                 };
 
                 let Some(value) = value else {
-                    return fail(self, DeleteOnboardingSecretError::NotFound);
+                    return fail(self, DeleteSecretError::NotFound);
                 };
                 let record = match postcard::from_bytes::<OnboardingSecretRecord>(&value) {
                     Ok(record) => record,
                     Err(error) => {
-                        return fail(
-                            self,
-                            DeleteOnboardingSecretError::ConversionError(error.into()),
-                        );
+                        return fail(self, DeleteSecretError::ConversionError(error.into()));
                     }
                 };
 
-                self.state = DeleteOnboardingSecretState::DeleteRecord {
+                self.state = DeleteSecretState::DeleteRecord {
                     txn_id,
                     record: record.clone(),
                 };
@@ -154,12 +151,12 @@ impl Operation for DeleteOnboardingSecretOperation {
                     txn_id: Some(txn_id),
                 })]
             }
-            DeleteOnboardingSecretState::DeleteRecord { txn_id, record } => {
+            DeleteSecretState::DeleteRecord { txn_id, record } => {
                 let got = format!("{event:?}");
                 let Event::Storage(StorageEvent::BatchDeleteResult { .. }) = event else {
                     return fail(
                         self,
-                        DeleteOnboardingSecretError::UnexpectedEvent {
+                        DeleteSecretError::UnexpectedEvent {
                             state: format!("{:?}", self.state),
                             expected: "batch delete result",
                             got,
@@ -167,17 +164,17 @@ impl Operation for DeleteOnboardingSecretOperation {
                     );
                 };
 
-                self.state = DeleteOnboardingSecretState::CommitTransaction {
+                self.state = DeleteSecretState::CommitTransaction {
                     record: record.clone(),
                 };
                 smallvec![Effect::Storage(StorageEffect::CommitTransaction { txn_id })]
             }
-            DeleteOnboardingSecretState::CommitTransaction { record } => {
+            DeleteSecretState::CommitTransaction { record } => {
                 let got = format!("{event:?}");
                 let Event::Storage(StorageEvent::TransactionCommitted { .. }) = event else {
                     return fail(
                         self,
-                        DeleteOnboardingSecretError::UnexpectedEvent {
+                        DeleteSecretError::UnexpectedEvent {
                             state: format!("{:?}", self.state),
                             expected: "transaction committed",
                             got,
@@ -185,32 +182,31 @@ impl Operation for DeleteOnboardingSecretOperation {
                     );
                 };
 
-                self.state = DeleteOnboardingSecretState::Finish;
+                self.state = DeleteSecretState::Finish;
                 self.output = Some(Ok(record));
                 smallvec![]
             }
-            DeleteOnboardingSecretState::Init
-            | DeleteOnboardingSecretState::Finish
-            | DeleteOnboardingSecretState::Error => smallvec![],
+            DeleteSecretState::Init | DeleteSecretState::Finish | DeleteSecretState::Error => {
+                smallvec![]
+            }
         }
     }
 
     fn is_complete(&self) -> bool {
         matches!(
             self.state,
-            DeleteOnboardingSecretState::Finish | DeleteOnboardingSecretState::Error
+            DeleteSecretState::Finish | DeleteSecretState::Error
         )
     }
 
     fn finalize(self) -> Result<Self::Output, Self::Error> {
-        self.output
-            .ok_or(DeleteOnboardingSecretError::NotFinished)?
+        self.output.ok_or(DeleteSecretError::NotFinished)?
     }
 
     fn abort(&mut self) -> Effects {
         match self.state {
-            DeleteOnboardingSecretState::ReadRecord { txn_id }
-            | DeleteOnboardingSecretState::DeleteRecord { txn_id, .. } => {
+            DeleteSecretState::ReadRecord { txn_id }
+            | DeleteSecretState::DeleteRecord { txn_id, .. } => {
                 smallvec![Effect::Storage(StorageEffect::AbortTransaction { txn_id })]
             }
             _ => smallvec![],
@@ -218,12 +214,9 @@ impl Operation for DeleteOnboardingSecretOperation {
     }
 }
 
-fn fail(
-    operation: &mut DeleteOnboardingSecretOperation,
-    error: DeleteOnboardingSecretError,
-) -> Effects {
+fn fail(operation: &mut DeleteSecretOperation, error: DeleteSecretError) -> Effects {
     let cleanup = operation.abort();
-    operation.state = DeleteOnboardingSecretState::Error;
+    operation.state = DeleteSecretState::Error;
     operation.output = Some(Err(error));
     cleanup
 }
