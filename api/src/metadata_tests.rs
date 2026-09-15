44547f0cb471edb9f19586cfe1a4d3b07134b41f
Sebastian Beyvers <sb@pus.de>
2026-09-15 15:49:36 +0200

    refactor: flatten the metadata API modules tests

--- api/src/metadata_tests.rs
+++ api/src/metadata_tests.rs
@@ -2,7 +2,7 @@
 //! realm/group permission gate.
 
 use super::*;
-use crate::tests::fixtures::routes::{seed_realm_auth, test_context, test_state, test_storage};
+use crate::tests::routes::{seed_realm_auth, test_context, test_state, test_storage};
 use aruna_core::structs::{Actor, NodeCapabilities, RealmId};
 use ed25519_dalek::SigningKey;
 use std::sync::Arc;
